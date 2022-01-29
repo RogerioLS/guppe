ede5e475c892bc13d1a4fb00b18ae3d146a5a1ad
Rogerio <rogerio_288@hotmail.com>
2022-01-29 16:53:46 -0300

    falando-sobre-classe

---
+++ secao16/classes.py
@@ -0,0 +1,18 @@
+"""
+POO - Classes
+
+Em POO, são classes nada mais são do que modelos dos objetos do mundo real sendo representados
+computacionalmente.
+
+Classes podem conter:
+    - Atributos -> Representam as características do objeto. Ou seja, pelos atributos consrguimos
+    representar computacionalmente os estados de um objeto. No caso da lámpada, possívelmente
+    iriamos querer saber se a lámpada é 110 ou 220 volts, se ela é branca, amarela, vermelha ou
+    outro cor, qual é a luminosidade dela e etc.
+
+    - Métodos (funções) -> Representam os comportamentos do objeto. Ou seja, as ações que este
+    objeto pode realizar no seu sistema. No caso da lampada, por exemplo, um comportamento comum
+    que muito provavelmente iriamos querer representar no nosso sistema é o de ligar e desligar
+    a mesma.
+"""
+
