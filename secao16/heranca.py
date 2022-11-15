a7d1a08e35b9414bfb3a32098df6c30b0f208bac
Merovingio <rogerio_288@hotmail.com>
2022-11-15 12:37:59 -0300

    heranca

---
+++ secao16/heranca.py
@@ -0,0 +1,45 @@
+"""
+POO - Herança(Inheritance)
+
+A ideia de herança é a de reaproveitar código.Também extender nossas classes.
+
+OBS: Com a herança, a partir de uma classe existente, nós extendemos outra classe
+que passa a herdar atributos e métodos da classe herdada.
+
+Cliente
+    - nome;
+    - sobrenome;
+    - cpf;
+    - renda;
+
+Funcionario
+    - nome;
+    - sobrenome;
+    - cpf;
+    - matricula;
+"""
+
+
+class Cliente:
+
+    def __init__(self, nome, sobrenome, cpf, renda):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+        self.__renda = renda
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
+
+class Funcionarios:
+
+    def __init__(self, nome, sobrenome, cpf, matricula):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__cpf = cpf
+        self.__matricula = matricula
+
+    def nome_completo(self):
+        return f'{self.__nome} {self.__sobrenome}'
+
