e92d65c04337ada3e3c28075bfc5d8f8403235ae
Merovingio <rogerio_288@hotmail.com>
2022-12-10 16:11:59 -0300

    propriedades

--- secao16/propriedades.py
+++ secao16/propriedades.py
@@ -6,6 +6,39 @@ costumamos a criar métodos públicos para manipulação desses atríbutos. Esse
 conhecidos por gatters e setters, onde os getters retornam o valor do atributo e os setters
 alteram o valor do mesmo.
 
+    def get_numero(self):
+        return self.__numero
+
+    def get_titular(self):
+        return self.__titular
+
+    def set_titular(self, titular):
+        self.__titular = titular
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
+
+soma = conta1.get_saldo() + conta2.get_saldo()
+print(f'A soma do saldos das contas é {soma}')
+
+print(conta1.__dict__)
+conta1.set_limite(99999)
+print(conta1.__dict__)
+
+* essa maneira vista de acima de get e set é uma maneira bem comum no java
+em python vamos utilizar as PROPRIEDADES para gerenciamento disso.
+
+- Declara a clasee
+- Atributos de classe
+- Construtores e seus atributos de instacia
+- Property
+- Metodos de intância
 """
 
 
@@ -20,6 +53,26 @@ class Conta:
         self.__limite = limite
         Conta.contador += 1
 
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, novo_limite):
+        self.__limite = novo_limite
+
     def extrato(self):
         return f'Saldo de {self.__saldo} do cliente {self.__titular}'
 
@@ -33,17 +86,9 @@ class Conta:
         self.__saldo -= valor
         destino.__saldo += valor
 
-    def get_numero(self):
-        return self.__numero
-
-    def get_titular(self):
-        return self.__titular
-
-    def get_saldo(self):
-        return self.__saldo
-
-    def get_limite(self):
-        return self.__limite
+    @property
+    def valor_total(self):
+        return self.__saldo + self.__limite
 
 
 conta1 = Conta('Teste1', 3000, 5000)
@@ -53,5 +98,13 @@ print(conta1.extrato())
 print(conta2.extrato())
 
 #soma = conta1._Conta__saldo + conta2._Conta__saldo
-soma = conta1.get_saldo() + conta2.get_saldo()
+soma = conta1.saldo + conta2.saldo
 print(f'A soma do saldos das contas é {soma}')
+
+print(conta1.__dict__)
+conta1.limite = 789456
+print(conta1.__dict__)
+print(conta1.limite)
+
+print(conta1.valor_total)
+print(conta2.valor_total)
