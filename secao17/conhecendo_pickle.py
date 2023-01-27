44f5d8c7c3ab05c3b29d475fff06d4a085193ce1
Merovingio <rogerio_288@hotmail.com>
2023-01-26 22:49:03 -0300

    conhecendo_pickle

---
+++ secao17/conhecendo_pickle.py
@@ -0,0 +1,16 @@
+"""
+Conhecendo o Pickle
+
+A funcao do pickle é realizar o seguinte processo:
+
+Objeto Python -> Binarização
+
+Binarização -> Objeto Python
+
+Este processo é chamado de serialização/deserialização
+
+#OBS: O módulo Pickle não é seguro contra dados maliciosos e deste forma
+não é recomendado trabalhar com arquivos pickle vindos de outras pessoas
+que você não conheça ou de fontes desconhecidas.
+
+"""
