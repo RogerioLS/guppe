c0a1bdbdabdbbdba68903612a6fb5634424a22fe
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-03-18 18:20:05 +0000

    intro

---
+++ secao19/intro_unittest.py
@@ -0,0 +1,20 @@
+"""
+Introdução ao modulo Unittest
+
+Unittest -> Testes Unitários
+
+O que são teste unitários?
+
+Teste é a forma de se testar unidades individuais de códigos fonte.
+Unidades individuais podem ser: funções, métodos, classes, módulos etc.
+
+#OBS: Teste unitários não é especifico da linguagem Python.
+Para criar nossos testes, criamos classes que herdam de unittest. TestCase
+e a partir de então ganhamos todos os 'assertions' presentes no módulo.
+
+Para rodar os testes, utilizamos unittest.main()
+
+TestCase -> Casos de teste para sua unidade
+Por convencão, todos os testes em um test case, devem ter seu nome
+iniciado com test_
+"""
